3d93b8be4f887015c18a69901dc2127d1abb63b9
RARH11 <121983496+RARH11@users.noreply.github.com>
2023-01-15 20:16:38 -0600

    Update Nema PS3.py

--- Nema PS3.py
+++ Nema PS3.py
@@ -1,42 +1,43 @@
-Import RPi.GPIO as GPIO
-Import time 
-Import pygame  
+Import RPi.GPIO as GPIO  #libreria RPi.GPIO
+Import time              #libreira time    
+Import pygame            #libreria pygame, permite vincular el control de PS3
 
-dire=16 
-step=20
-cw=-1 
-ccw=-1 
+dire=16                  #habilitar pin 16 
+step=20                  #habilitar pin 20
+cw=-1                    #Variable cw
+ccw=-1                   #Variable ccw
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)  #habilitar pines en formato BCM
 GPIO.setwarnings(false)
-GPIO.setup(dire, GPIO.OUT) 
-GPIO.setup(step, GPIO.OUT) 
+GPIO.setup(dire, GPIO.OUT) #habilitar pin en salidad
+GPIO.setup(step, GPIO.OUT) #habiliotar pin en salidad 
 GPIO.output(dire, cw) 
 
-Pygame.display.init()
-Pygame.joystick.init()
+Pygame.display.init()   #Inicializa el atributo display del mando PS3
+Pygame.joystick.init()  #Inicializa el atributo Joystick del mando PS3
 Pygame.joystick.Joystick(0).init() 
-pwmdire=GPIO.PWM(dire,  200)
+pwmdire=GPIO.PWM(dire,  200)    
 pwmstep=GPIO.PWM(step, 200) 
-pwmdire.start(0)
+pwmdire.start(0)      #inicio del motor
 pwmstep.start(0)
-pygame.init()
-j=pygame.joystick.Joystick(0)
+pygame.init()         #inicio del mando PS3 
+j=pygame.joystick.Joystick(0)  #valores del mando de PS3 en 0
 j.init()
 
 try: 
      while True: 
           pygame.event.pump()
-          if  j.get_axis(1)>0.2) or (j.get_axis(1)<-0.2):
+          if  j.get_axis(1)>0.2) or (j.get_axis(1)<-0.2):     #Condicion del joystick 1 para activar 
+                                                   #solo cuando esta a mas del 20 por ciento de su valores 
              if  j.get_axis(1)<-0.2: 
-                  GPIO.output(dire,cw)
+                  GPIO.output(dire,cw)     #giro al sentido del reloj 
                   GPIO.output(step, GPIO.HIGH)
                   GPIO.outut(step, GPIO.LOW) 
              if  j.get_axis(1)>0.2: 
-                 GPIO.output(dire,ccw,)
+                 GPIO.output(dire,ccw,)   #giro al sentido contrario del reloj 
                   GPIO.output(step, GPIO.HIGH)
                   GPIO.outut(step, GPIO.LOW) 
 
-GPIO.cleanup()
+GPIO.cleanup()               #Finaliza los pines 
 Pwmstep.stop()
 Pwmdire.stop() 
